d0b0cce2fd5c4b64086e47cfe8706674358278ba
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-03-31 04:01:50 +0530

    Added delivery and stock to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,13 @@ export default function Card({ products }) {
     <div class="cards card-ecom">
       {products.map((item) => {
         return (
-          <div class="card">
+          <div class="card" key={item.id}>
             <img class="card-image" alt="NA" src={item.image} />
             <div class="card-content card-content-ecom">
               <div>{item.ratings}</div>
               <h3>{item.name}</h3>
+              <h3>{item.delivery}</h3>
+              <h3>{item.inStock}</h3>
               <p class="card-data card-data">{item.price}</p>
             </div>
           </div>
