5310c92841336efeb1ead2d200ce0caba75dc0af
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-03-30 19:40:04 +0530

    Added routing logic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import products from "./data/products";
 import Card from "./components/Card";
 import Nav from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
+import Products from "./components/Products";
+import { useState } from "react";
 
 function App() {
+  const [route, setRoute] = useState("landing");
   return (
     <div className="App">
-      <Nav />
-      <LandingPage />
-      {/* <Card products={products} /> */}
+      <Nav route={route} setRoute={setRoute} />
+      {route === "landing" && <LandingPage />}
+      {route === "products" && <Products />}
+      {route === "cart" && <Card products={products} />}
     </div>
   );
 }
