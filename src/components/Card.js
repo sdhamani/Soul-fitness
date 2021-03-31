80622ce7510926aab2e91d592f368c4dc01a7ec3
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-03-31 23:26:45 +0530

    Updated AddtoCart Logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,49 @@
 import "./components.css";
-
+import useCart from "../context/cart-context";
+import useData from "../context/data-context";
 export default function Card({ products }) {
+  const { cart, dispatch } = useCart();
+  const { data, setData } = useData();
   return (
-    <div class="cards card-ecom">
+    <div className="cards card-ecom">
       {products.map((item) => {
         return (
-          <div class="card" key={item.id}>
-            <img class="card-image" alt="NA" src={item.image} />
-            <div class="card-content card-content-ecom">
+          <div className="card" key={item.id}>
+            <img
+              className="card-image text-overlay-image"
+              alt="NA"
+              src={item.image}
+            />
+            <div className="card-overlay-data">
+              <button
+                className="cart-image"
+                onClick={(e) => {
+                  data.map((value) => 
+                    value.id === item.id ? (setData({...data,value.addedToCart: true})):null 
+                  );
+                  return dispatch({ type: "ADDTOCART", payload: item });
+                }}
+              >
+                {item.addedToCart ? (
+                  <i class="fa fa-check-circle" aria-hidden="true"></i>
+                ) : (
+                  <i
+                    className="fa fa-shopping-cart fa-2x"
+                    aria-hidden="true"
+                  ></i>
+                )}
+              </button>
+
+              <button className="cart-image">
+                <i className="fa fa-heart-o fa-2x"></i>
+              </button>
+            </div>
+            <div className="card-content card-content-ecom">
               <div>{item.ratings}</div>
               <h3>{item.name}</h3>
               <h3>{item.delivery}</h3>
               <h3>{item.inStock}</h3>
-              <p class="card-data card-data">{item.price}</p>
+              <p className="card-data card-data">{item.price}</p>
             </div>
           </div>
         );
