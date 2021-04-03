bc334a5ac2e2716c74d8b3c899bbc227c9a331ed
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-03 22:28:46 +0530

    Added routes for cart  and wishlist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
 import products from "./data/products";
-import Card from "./components/Card";
+import Cart from "./components/Cart";
 import Nav from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
 import Products from "./components/Products";
 import { useState } from "react";
+import Wishlist from "./components/Wishlist";
 
 function App() {
   const [route, setRoute] = useState("landing");
+  console.log(route);
   return (
     <div className="App">
       <Nav route={route} setRoute={setRoute} />
       {route === "landing" && <LandingPage />}
       {route === "products" && <Products />}
-      {route === "cart" && <Card products={products} />}
+      {route === "cart" && <Cart />}
+      {route === "wishlist" && <Wishlist />}
     </div>
   );
 }
