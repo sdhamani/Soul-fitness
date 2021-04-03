279d697f3becac61143c49cab6a60146327bcc4d
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-04 04:01:27 +0530

    Added icons to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
 import "./components.css";
+import useCart from "../context/cart-context";
+import useWishlist from "../context/wishlist-context";
 export default function Nav({ route, setRoute }) {
+  const { cart, dispatch } = useCart();
+  const { wishlist, wishlistdispatch } = useWishlist();
   return (
     <nav className="navigation nav-ecom">
+      <h1 className="nav-heading">Soul Fitness</h1>
       <ul className="list-no-bullets nav-pills nav-list-ecom">
         <li className="list-item-inline">
           <button
@@ -10,7 +15,7 @@ export default function Nav({ route, setRoute }) {
               setRoute("landing");
             }}
           >
-            Home
+            <i class="fa fa-home fa-lg badge-icons" aria-hidden="true"></i>
           </button>
         </li>
         <li className="list-item-inline">
@@ -20,7 +25,7 @@ export default function Nav({ route, setRoute }) {
               setRoute("products");
             }}
           >
-            Products
+            <i class="fa fa-product-hunt" aria-hidden="true"></i>
           </button>
         </li>
         <li className="list-item-inline">
@@ -28,7 +33,14 @@ export default function Nav({ route, setRoute }) {
             className={route === "wishlist" ? "link link-active" : "link"}
             onClick={(e) => setRoute("wishlist")}
           >
-            Wishlist
+            <div className="badge-div">
+              <i class="fa fa-heart fa-lg badge-icons" aria-hidden="true">
+                {" "}
+                <span class="badge-on-icon badge-on-icon-ecom">
+                  {wishlist.length > 0 && wishlist.length}
+                </span>
+              </i>
+            </div>
           </button>
         </li>
         <li className="list-item-inline">
@@ -36,7 +48,17 @@ export default function Nav({ route, setRoute }) {
             className={route === "cart" ? "link link-active" : "link"}
             onClick={(e) => setRoute("cart")}
           >
-            Cart
+            <div className="badge-div">
+              <i
+                class="fa fa-shopping-cart fa-lg badge-icons"
+                aria-hidden="true"
+              >
+                {" "}
+                <span class="badge-on-icon badge-on-icon-ecom">
+                  {cart.length > 0 && cart.length}
+                </span>
+              </i>
+            </div>
           </button>
         </li>
       </ul>
