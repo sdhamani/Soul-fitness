845415295150f2904ea9923990a0daa456b11535
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-06 00:47:11 +0530

    Added feature product component

---
+++ src/components/FeatureProducts.js
@@ -0,0 +1,26 @@
+import "./components.css";
+
+import useData from "../context/data-context";
+
+export default function FeatureProducts({ products }) {
+  return (
+    <div className="cards cards-ecom">
+      {products.map((item) => {
+        return (
+          <div className="card  card-ecom" key={item.id}>
+            <img
+              className="card-image text-overlay-image card-image-ecom"
+              alt="NA"
+              src={item.image}
+            />
+            <div className="card-content card-content-ecom">
+              <div>{item.ratings}</div>
+              <h4>{item.name}</h4>
+              <p className="card-data card-data">${item.price}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
