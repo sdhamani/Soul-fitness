6c71a8e938bb637ff48278754ed512ea2f2881a0
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-25 21:42:41 +0530

    Added route for signup and login

--- src/App.js
+++ src/App.js
@@ -12,24 +12,30 @@ import Footer from "./components/Footer";
 import Equipments from "./components/Equipments";
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [route, setRoute] = useState("landing");
-  console.log(route);
+  const [login, setLogin] = useState(false);
   return (
     <div className="App">
       <Nav route={route} setRoute={setRoute} />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<Products />} />
+        {true && <Route path="/wishlist" element={<Login />} />}
+        {true && <Route path="/cart" element={<Login />} />}
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<LandingPage />} />
         <Route path="/mens" element={<MensProducts />} />
         <Route path="/womens" element={<Womensproducts />} />
         <Route path="/equipments" element={<Equipments />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </div>
