b5afd45ca7149ec791d71853ae595f77ccfab5ec
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-28 05:28:13 +0530

    Updated get wishlist state

--- src/context/wishlist-context.js
+++ src/context/wishlist-context.js
@@ -9,22 +9,27 @@ export default function useWishlist() {
 export function WishlistProvider({ children }) {
   const dispatchFunc = (state, value) => {
     const obj = value.payload;
-
-    if (value.type === "UPDATEWISHLIST") {
-      if (obj.addedToWishlist === false) {
-        return [
-          ...state,
-          {
-            id: obj.id,
-            name: obj.name,
-            quantity: 1,
-            price: obj.price,
-            image: obj.image,
-          },
-        ];
-      } else {
-        return state.filter((item) => item.id !== obj.id);
-      }
+    switch (value.type) {
+      case "USERWISHLIST":
+        console.log("USERWISHLIST");
+        return value.payload;
+      case "UPDATEWISHLIST":
+        if (obj.addedToWishlist === false) {
+          return [
+            ...state,
+            {
+              id: obj.id,
+              name: obj.name,
+              quantity: 1,
+              price: obj.price,
+              image: obj.image,
+            },
+          ];
+        } else {
+          return state.filter((item) => item.id !== obj.id);
+        }
+      default:
+        return state;
     }
   };
 
