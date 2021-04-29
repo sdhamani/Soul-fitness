6342bce111b0cce17996e4c37f61267bfa7fa9b7
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-29 05:44:56 +0530

    Updated cart api

--- src/api/cart-api.js
+++ src/api/cart-api.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 
+export async function UpdateCartQuantiyAPI(token, productId, quantity) {
+  console.log("Update quantity running in api", token);
+  const url = `http://localhost:3000/cart/${productId}/${quantity}`;
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": token,
+      },
+    };
+    const cartObj = await axios.post(url, null, config);
+    console.log({ cartObj });
+    if (cartObj.data.success) {
+      return { success: true, updatedCart: cartObj.data.Updatedcart };
+    } else {
+      return { success: false, message: cartObj.data.message };
+    }
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
+
 export default async function Getcart(token) {
+  console.log("Getting cart");
   const url = "http://localhost:3000/cart";
   try {
     const config = {
@@ -23,7 +46,8 @@ export default async function Getcart(token) {
   }
 }
 
-export async function AddToCart(token, productId) {
+export async function AddToCartAPI(token, productId) {
+  console.log("ATC running in api", token);
   const url = `http://localhost:3000/cart/${productId}`;
   try {
     const config = {
@@ -33,6 +57,28 @@ export async function AddToCart(token, productId) {
       },
     };
     const cartObj = await axios.post(url, null, config);
+    console.log({ cartObj });
+    if (cartObj.data.success) {
+      return { success: true, updatedCart: cartObj.data.Updatedcart };
+    } else {
+      return { success: false, message: cartObj.data.message };
+    }
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
+
+export async function DeletFromCartAPI(token, productId) {
+  console.log("DFC running in api", token);
+  const url = `http://localhost:3000/cart/${productId}`;
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": token,
+      },
+    };
+    const cartObj = await axios.delete(url, config);
     console.log({ cartObj });
     if (cartObj.data.success) {
       return { success: true, updatedCart: cartObj.data.Updatedcart };
