4b5e0da3d7596163ebb5cb9dbd19dbccab9d076f
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-29 05:47:35 +0530

    Updated Cart context

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,8 +1,17 @@
-import { createContext, useContext, useReducer, useState } from "react";
-import { AddToCart } from "../api/cart-api";
+import {
+  createContext,
+  useContext,
+  useReducer,
+  useState,
+  useEffect,
+} from "react";
+import { AddToCartAPI } from "../api/cart-api";
 
 import useData from "./data-context";
 import useLogin from "./login-context";
+import useWishlist from "./wishlist-context";
+import Getwishlist from "../api/wishlist-api";
+import Getcart from "../api/cart-api";
 
 const CartContainer = createContext();
 
@@ -11,68 +20,31 @@ export default function useCart() {
 }
 
 export function CartProvider({ children }) {
-  const { token } = useLogin();
   const [initalcart, setInitialCart] = useState([]);
-  const { data, setData } = useData();
+  const { wishlistdispatch } = useWishlist();
+  const { userName, setuserName } = useLogin();
 
-  const addtocart = async (token, productid) => {
-    const response = await AddToCart(token, productid);
-    if (response.success) {
-      return { success: true, updatedCart: response.updatedCart };
-    } else {
-      return { success: false, message: response.message };
+  useEffect(() => {
+    const { token } = JSON.parse(localStorage?.getItem("token"));
+
+    const getCartAndWishlist = async () => {
+      const apicart = await Getcart(token);
+      dispatch({ type: "USERCART", payload: apicart });
+      const apiwishlist = await Getwishlist(token);
+      wishlistdispatch({ type: "USERWISHLIST", payload: apiwishlist });
+    };
+    if (JSON.parse(localStorage?.getItem("login"))) {
+      getCartAndWishlist();
     }
-  };
+  }, []);
 
   const dispatchFunc = (state, value) => {
     const obj = value.payload;
-    console.log({ obj });
     switch (value.type) {
       case "USERCART":
-        return value.payload;
-      case "ADDTOCART":
-        const response = addtocart(token, obj._id);
-        if (response.success) {
-          return (state = response.updatedCart);
-        } else {
-          return state;
-        }
-      case "ADDQUANTITY":
-        const newArrayAdd = state.map((item) => {
-          if (item.id === value.payload.id) {
-            return { ...item, quantity: item.quantity + 1 };
-          }
-          return item;
-        });
+        console.log({ obj });
+        return obj;
 
-        return newArrayAdd;
-      case "SUBSQUANTITY":
-        const newArraysSub = state.map((item) => {
-          if (item.id === value.payload.id) {
-            return {
-              ...item,
-              quantity: item.quantity === 1 ? item.quantity : item.quantity - 1,
-            };
-          }
-          return item;
-        });
-
-        return newArraysSub;
-      case "DELFROMCART":
-        console.log("Casedele");
-        console.log({ data });
-        console.log({ value });
-        const updatedArray = data.map((item) => {
-          console.log("item.id-", item.id);
-          console.log("value.payload.id-", value.payload.id);
-          if (item.id === value.payload.id) {
-            return { ...item, addedToCart: false };
-          } else {
-            return item;
-          }
-        });
-        setData(updatedArray);
-        return state.filter((item) => item.id !== value.payload.id);
       default:
         return state;
     }
