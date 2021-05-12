3237f01d33b27d55985730bf95d029019ec419ff
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-12 11:36:04 +0530

    Added 404 Page

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./components.css";
+
+function PageNotFound() {
+  return (
+    <div className="PageNotFound">
+      <div>
+        <i class="fa fa-frown-o fa-5x" aria-hidden="true"></i>
+      </div>
+      <h1>Page Not Found</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
