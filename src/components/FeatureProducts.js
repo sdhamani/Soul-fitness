44108f309305c51c0e269450745c8b30a17f2df8
sdhamani <sagar.dhamani1@gmail.com>
2021-07-29 01:23:32 +0530

    Added card to feature components

--- src/components/FeatureProducts.js
+++ src/components/FeatureProducts.js
@@ -1,24 +1,9 @@
 import "./components.css";
-
+import Card from "./Card";
 export default function FeatureProducts({ products }) {
   return (
     <div className="cards cards-ecom">
-      {products.map((item) => {
-        return (
-          <div className="card  card-ecom" key={item._id}>
-            <img
-              className="card-image text-overlay-image card-image-ecom"
-              alt="NA"
-              src={item.image}
-            />
-            <div className="card-content card-content-ecom">
-              <div>{item.ratings}</div>
-              <h4>{item.name}</h4>
-              <p className="card-data card-data">${item.price}</p>
-            </div>
-          </div>
-        );
-      })}
+      <Card products={products} />
     </div>
   );
 }
